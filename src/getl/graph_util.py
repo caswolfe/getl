c95f105b98629473f4ac09ac4c068b6291febc76
Chad-Andrew Wolfe <chadandrewwolfe@gmail.com>
2021-11-25 21:40:15 -0500

    i'm big dumb, didn't need that

--- src/getl/graph_util.py
+++ src/getl/graph_util.py
@@ -10,14 +10,6 @@ def get_process_starting_at(graph: networkx.DiGraph, node: Any):
     subset_graph = graph.copy()
 
     while len(subset_exclude) > 0:
-        n = subset_exclude.pop()
-        se_children = list(networkx.descendants(subset_graph, n))
-
-        # If there is an intersection in the children of the "node to remove", and
-        # the subset of "nodes to include", it is a parent of a node to include.
-        if len([i for i in se_children if i in subset_include]) > 0:
-            subset_include.append(n)
-        else:
-            subset_graph.remove_node(n)
+        subset_graph.remove_node(subset_exclude.pop())
 
     return subset_graph
